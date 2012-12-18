880b1905e2f16ab1f7964e22db22a309cdde45a2
Pawel Preneta <jusuff@jabster.pl>
2012-12-18 05:17:29 +0100

    Leszek Zyczkowski suggestions

--- formlet@preneta.pl/defaults/preferences/prefs.js
+++ formlet@preneta.pl/defaults/preferences/prefs.js
@@ -1,8 +1,3 @@
-// Default preference values. These are accessible via the preferences system
-// or via the optional chrome/content/options.xul preferences dialog.
-
-// These are here as an example only. Please remove them or rename them to
-// something useful.
 pref('extensions.formlet.showDialog', true);
 pref('extensions.formlet.saveBlank', true);
 pref('extensions.formlet.saveHidden', true);
@@ -10,6 +5,3 @@ pref('extensions.formlet.savePasswords', false);
 pref('extensions.formlet.titlePattern', 'FormLet - {TITLE} - {DATE}');
 pref('extensions.formlet.showBookmarksDialog', true);
 pref('extensions.formlet.bookmarksFolder', 0);
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref('extensions.formlet.description', 'chrome://formlet/locale/formlet.properties');
